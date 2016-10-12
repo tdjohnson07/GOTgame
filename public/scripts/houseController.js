db19312fe22a130996ee542534c08636d9f8c59d
Trent Johnson <tdjohnson0722@gmail.com>
2016-10-12 17:00:23 -0500

    sweet alerts added

--- public/scripts/houseController.js
+++ public/scripts/houseController.js
@@ -2,4 +2,21 @@ angular.module('GOTapp').controller('houseController',['$location','houseService
   var vm = this;
   vm.data = houseService.data;
   console.log(vm.data);
+  vm.arryn= function(){
+    console.log('clicked');
+    swal({
+  title: 'Are you sure?',
+  text: 'House Arryn, Family Members: Lord: Jon Arryn, Wife: Lysa (Tully), Childern: Robin',
+  type: 'info',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Start with House Arryn?'
+}).then(function() {
+  swal(
+    'House Arryn Selected',
+    'success'
+  );
+})
+  }
 }]);
